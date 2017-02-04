f27c31fd9f06394c010c584799aa52952b188616
Donn-Eddy <maindric@live.com>
2017-02-04 01:50:16 -0500

    Success!
    Stored array added to store results from the sequence.  This sped up
    calculations tramendously.  The algorythm is still technically a complex
    problem, but operates much close to a simple one.  Unfortunately, I do
    not think you can simplyfy the sequence much further.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@ using namespace std;
 
 int Fib(int x);
 
+//This would best be served as a Linked List to dynamically allocate spots for memory efficiency, however 100 spots in an array is not that inefficient.
+//However, for a 32-bit integer it could be calculated that after 47 iterations it's useless as the value wraps around.  I did this as a proof of concept in terms of speed.
+//It takes less than a quarter of a second to perform 100 iterations with a stored array to return values.
+int FibStore[100];
+
 int main()
 {
     int iterations = 0;
-    cout << "Greetings!  How many cycle of Fibonacci's sequence are we going through today?" << endl;
+    cout << "Greetings!  How many cycle of Fibonacci's sequence are we going through today? (Max 99)" << endl;
     cin >> iterations;
     cout << endl;
-    for (int i = 0; i <= iterations; i++)
+    if (iterations > 99)
     {
-        int num = Fib(i);
-        //Ternary operator used to differentiate between iterations.  The check is to ensure no comma is awkwardly placed after the last value.
-        cout << num << ((i==iterations)? "" : ",");
+        cout << "I said 99 max, you little devil!" << endl;
+    } else
+    {
+        for (int i = 0; i <= iterations; i++)
+        {
+            int num = Fib(i);
+            //Ternary operator used to differentiate between iterations.  The check is to ensure no comma is awkwardly placed after the last value.
+            cout << num << ((i==iterations)? "" : ",");
+        }
     }
     return 0;
 }
@@ -25,9 +36,13 @@ int Fib(int x)
     int res = (x == 0) ? 0 : 1;
 
     //Fibonacci's sequence in it's most basic form.  Fn = Fn-1 + Fn-2
-    if (x >= 3)
+    if (FibStore[x] != 0)
+    {
+        res = FibStore[x];
+    } else if (x >= 3)
     {
         res = Fib(x-1) + Fib(x-2);
+        FibStore[x] = res;
     }
 
     return res;
